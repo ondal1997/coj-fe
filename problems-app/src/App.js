7f46b58ca3b7e73a3b8f15561a6a1fb41605e1d6
Hyun-ees <migu554@naver.com>
2021-03-24 15:49:49 +0900

    [수정] Route 관련 기능 추가
    - react-router-dom 모듈 사용
    - Nav 컴포너트 렌더링

---
+++ problems-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProblemListApp from './ProblemListApp';
+import SolutionListApp from './SolutionListApp';
+import ProblemFormApp from './ProblemFormApp';
+import Nav from './components/Nav';
+
+const Home = () => (
+  <div>
+  홈입니당
+  </div>
+);
+
+const App = () => (
+  <Router>
+    <Nav />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/solutions/:problemNo" component={SolutionListApp} />
+      <Route path="/problems" component={ProblemListApp} />
+      <Route path="/problemsForm" component={ProblemFormApp} />
+    </Switch>
+  </Router>
+);
+
+export default App;
