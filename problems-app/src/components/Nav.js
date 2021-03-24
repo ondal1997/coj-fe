a79626b33d48a2d342a7d24c45bbf8ffb29de846
Hyun-ees <migu554@naver.com>
2021-03-24 16:01:43 +0900

    [추가] Nav 컴포넌트
    - Link api 사용

---
+++ problems-app/src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <Link to="/">
+      <li>Home</li>
+      </Link>
+      <Link to="/solutions">
+      <li>풀이 제출 현황</li>
+      </Link>
+      <Link to="/problems">
+      <li>문제 리스트</li>
+      </Link>
+      <Link to="/problemsForm">
+      <li>문제 제출</li>
+      </Link>
+    </ul>
+  </nav>
+);
+
+export default Nav;
