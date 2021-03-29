191a1598042be99bacb4ba78563bd52beabd7dc2
Hyun-ees <migu554@naver.com>
2021-03-29 14:16:05 +0900

    [수정] 채점상태 추가

--- src/SolutionList/JudgeState.js
+++ src/SolutionList/JudgeState.js
@@ -1,4 +1,4 @@
-const JudgeState = [
+const judgeState = [
   {
     color: '#000000',
     name: '대기 중',
@@ -9,12 +9,46 @@ const JudgeState = [
   },
   {
     color: '#283DF5',
-    name: 'Solve !!!',
+    name: 'Solve !',
   },
   {
     color: '#F02424',
     name: 'Try again ...',
   },
+  {
+    color: '#F02424',
+    name: '시간 초과 ...',
+  },
+  {
+    color: '#F02424',
+    name: '메모리 초과 ...',
+  },
+  {
+    color: '#F02424',
+    name: '컴파일 에러 ...',
+  },
+  {
+    color: '#F02424',
+    name: '런타임 에러 ...',
+  },
+  {
+    color: '#F02424',
+    name: '문제 유실 ...',
+  },
+  {
+    color: '#F02424',
+    name: '서버 실패 ...',
+  },
 ];
 
-export default JudgeState;
+// getStateByCode(code) {
+//   if (length code) {
+//     return [code]
+//   }
+//   color: defaultsDeep
+//   name: '미정의된 코드'
+// }
+// return
+// }
+
+export default judgeState;
