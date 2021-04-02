233e249841c7b17efe6689d4e4071b56af4cf6bb
Hyun-ees <migu554@naver.com>
2021-04-02 16:53:27 +0900

    [수정] 고정 height 스타일 제거

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import styled from 'styled-components';
 import ProblemListApp from './ProblemList/ProblemListApp';
 import SolutionListApp from './SolutionList/SolutionListApp';
 import SolutionListAppWrapper from './SolutionList/SolutionListAppWrapper';
@@ -10,27 +9,21 @@ import ProblemDetailApp from './ProblemDetail/ProblemDetailApp';
 import SolutionDetailApp from './SolutionDetail/SolutionDetailApp';
 import Nav from './Nav';
 
-const StyledApp = styled.div`
-  max-height: 500px;
-`;
-
 const App = () => (
   <>
-  <StyledApp>
     <Router basename="/oj">
       <Nav />
       <Switch>
         <Route path="/" exact component={ProblemListApp} />
         <Route path="/solutions" exact component={SolutionListApp} />
-        <Route path="/solutions/:problemNo" component={SolutionListAppWrapper} />
+        <Route path="/solutions/:problemNo/:problemTitle/:pageNum" component={SolutionListAppWrapper} />
         <Route path="/solution/:solutionKey" component={SolutionDetailApp} />
-        <Route path="/problems" component={ProblemListApp} />
+        <Route path="/problems/:pageNum" component={ProblemListApp} />
         <Route path="/problemsForm" component={ProblemFormApp} />
         <Route path="/solutionForm/:problemKey/:problemTitle" component={SolutionFormApp} />
         <Route path="/problem/:problemKey" component={ProblemDetailApp} />
       </Switch>
     </Router>
-  </StyledApp>
   </>
 );
 
