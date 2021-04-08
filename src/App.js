5f37ed431e2196884e5a89ec5e484ff4e0d447e1
eojin-dev <eojin-dev@naver.com>
2021-04-08 19:05:08 +0900

    [수정] 네비 제거, 홈 라우팅 변경

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProblemFormApp from './ProblemForm/ProblemFormApp';
 import SolutionFormApp from './SolutionForm/SolutionFormApp';
 import ProblemDetailApp from './ProblemDetail/ProblemDetailApp';
 import SolutionDetailApp from './SolutionDetail/SolutionDetailApp';
-import { OurLink } from './OurLink';
+// import { OurLink } from './OurLink';
 
 const theme = createMuiTheme({
   palette: {
@@ -17,30 +17,29 @@ const theme = createMuiTheme({
   },
 });
 
-const Nav = () => (
-  <nav>
-    <h3>==테스트용 네비게이션======</h3>
-    <ul>
-      <OurLink to="/allSolutions/1">
-      <li>전체 풀이 제출 현황</li>
-      </OurLink>
-      <OurLink to="/problems">
-      <li>문제 리스트</li>
-      </OurLink>
-      <OurLink to="/problemsForm">
-      <li>문제 제출</li>
-      </OurLink>
-    </ul>
-    <h3>=========================</h3>
-  </nav>
-);
+// const Nav = () => (
+//   <nav>
+//     <h3>==테스트용 네비게이션======</h3>
+//     <ul>
+//       <OurLink to="/allSolutions/1">
+//       <li>전체 풀이 제출 현황</li>
+//       </OurLink>
+//       <OurLink to="/problems">
+//       <li>문제 리스트</li>
+//       </OurLink>
+//       <OurLink to="/problemsForm">
+//       <li>문제 제출</li>
+//       </OurLink>
+//     </ul>
+//     <h3>=========================</h3>
+//   </nav>
+// );
 
 const App = () => (
   <ThemeProvider theme={theme} >
   <Router basename="/oj">
-    <Nav />
     <Switch>
-      <Route path="/" exact component={ProblemDetailApp} />
+      <Route path="/" exact component={ProblemListApp} />
       <Route path="/problemsForm" exact component={ProblemFormApp} />
       <Route path="/problems" exact component={ProblemListApp} />
       <Route path="/problem/:problemKey" exact component={ProblemDetailApp} />
