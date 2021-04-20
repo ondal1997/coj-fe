c9ff95c4e8a5d7db6640de7c41e5585e1c2540ae
eojin-dev <eojin-dev@naver.com>
2021-04-20 13:17:36 +0900

    [수정] 문제 상세 여백 조절

--- src/ProblemDetail/Problem.js
+++ src/ProblemDetail/Problem.js
@@ -147,7 +147,7 @@ const Problem = (props) => {
   }
 
   return isLoaded ? (
-    <StyledGrid container direction="column" spacing={3}>
+    <StyledGrid container direction="column" spacing={2}>
       <Grid item>
         <Grid container justify='flex-end'>
           {isOwners
@@ -205,7 +205,7 @@ const Problem = (props) => {
       </Grid>
 <Grid container item justify='space-between' alignItems='center' spacing={3}>
         <Grid item>
-          <Grid container direction="column" spacing={1}>
+          <Grid container direction="column" spacing={0}>
             <Grid item>
               <Grid container alignItems="center" direction="row" spacing={1}>
                 <Grid item>
