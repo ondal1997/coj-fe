69c3f6d82cce21047272f0564d5bb50adf4d8901
Hyeondoonge <migu554@naver.com>
2021-05-13 14:02:31 +0900

    [추가] 헤더 추가
    - PageTemplate 적용
    - 마이페이지 및 문제 리스트 페이지 이동 버튼

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import React, { useContext } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+} from 'react-router-dom';
+import { Button, ButtonGroup, IconButton, Tooltip } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { SnackbarProvider } from 'notistack';
+import {
+  AccountBoxRounded,
+  AccountCircleOutlined,
+  ListAltOutlined,
+  PermIdentity,
+} from '@material-ui/icons';
 import ProblemForm from './pages/ProblemForm';
 import SolutionForm from './pages/SolutionForm';
 import Problem from './pages/Problem';
@@ -11,8 +23,11 @@ import ProblemList from './pages/ProblemList';
 import SolutionList from './pages/SolutionList';
 import User from './pages/User';
 import { ErrorProvider } from '../contexts/error';
-import { AuthenticationProvider } from '../contexts/authentication';
+import AuthenticationContext, {
+  AuthenticationProvider,
+} from '../contexts/authentication';
 import Error from './pages/Error';
+import PageTemplate from './templates/PageTemplate';
 
 const theme = createMuiTheme({
   palette: {
@@ -25,22 +40,70 @@ const theme = createMuiTheme({
   },
 });
 
+const Header = () => {
+  const [userId] = useContext(AuthenticationContext);
+  const history = useHistory();
+
+  return (
+    <PageTemplate>
+      <div style={{ textAlign: 'right' }}>
+        <ButtonGroup variant="text">
+          <Tooltip title="마이페이지">
+            <IconButton
+              onClick={() => {
+                history.push(`/users/${userId}`);
+              }}
+              disabled={!userId}
+            >
+              <AccountBoxRounded />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="문제 리스트">
+            <IconButton
+              onClick={() => {
+                history.push('/');
+              }}
+            >
+              <ListAltOutlined />
+            </IconButton>
+          </Tooltip>
+        </ButtonGroup>
+      </div>
+    </PageTemplate>
+  );
+};
+
 const App = () => (
   <Router basename="/oj">
     <ThemeProvider theme={theme}>
-      <SnackbarProvider content={(key, message) => (<Button id={key} variant='contained' color='primary' size='large'>{message}</Button>)}>
+      <SnackbarProvider
+        content={(key, message) => (
+          <Button id={key} variant="contained" color="primary" size="large">
+            {message}
+          </Button>
+        )}
+      >
         <ErrorProvider>
           <AuthenticationProvider>
+            <Header />
             <Switch>
               <Route path={['/', '/problems']} exact component={ProblemList} />
               <Route path="/new" exact component={ProblemForm} />
               <Route path="/edit/:problemKey" exact component={ProblemForm} />
               <Route path="/problems/:problemKey" exact component={Problem} />
 
-              <Route path="/submit/:problemKey" exact component={SolutionForm} />
+              <Route
+                path="/submit/:problemKey"
+                exact
+                component={SolutionForm}
+              />
               <Route path="/solutions" exact component={SolutionList} />
-              <Route path="/solutions/:solutionKey" exact component={Solution} />
-              <Route path="/users/:id" exact component={User}/>
+              <Route
+                path="/solutions/:solutionKey"
+                exact
+                component={Solution}
+              />
+              <Route path="/users/:id" exact component={User} />
               <Route render={() => <Error error={{ status: 404 }} />} />
             </Switch>
           </AuthenticationProvider>
