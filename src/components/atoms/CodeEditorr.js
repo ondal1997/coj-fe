8392d7b46dce7f3ded6b2f36b828675a2a043183
Hyeondoonge <migu554@naver.com>
2021-06-03 12:56:11 +0900

    CodeEditorr.js

---
+++ src/components/atoms/CodeEditorr.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/python/python';
+import 'codemirror/mode/clike/clike';
+import '../../css/reset_solutionform.css';
+import { getMode } from '../../utils';
+
+const CodeEditor = ({ defaultValue, language, updateCode }) => {
+  const handleChange = (newCode) => {
+    // updateCode(newCode);
+  };
+
+  return (
+    <>
+      <CodeMirror value={defaultValue}
+        height={500}
+        options={{
+          mode: language ? getMode(language) : '',
+          lineNumbers: true,
+        }}
+        onChange={handleChange}
+        />
+    </>
+  );
+};
+
+export default CodeEditor;
